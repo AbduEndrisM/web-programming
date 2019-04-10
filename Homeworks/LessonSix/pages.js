19204a12478c73cd9f147d192b6db246eb67e087
AbduEndrisM <beazsasm@gmail.com>
2019-04-09 23:47:18 -0500

    Lesson Six - Home work - playing with pages

--- Homeworks/LessonSix/pages.js
+++ Homeworks/LessonSix/pages.js
@@ -1,6 +1,5 @@
 function pageOne() {
 
-
     var text = document.getElementById("txt");
     text.innerHTML =
         "<ul> " +
@@ -10,32 +9,49 @@ function pageOne() {
     ;
 
 }
+
 function pageTwo() {
-
-
     var text = document.getElementById("txt");
-    text.style.fontSize="1.5em";
-    text.style.textAlign="left";
+    text.style.fontSize = "1.5em";
+    text.style.textAlign = "left";
     // nav.style.padding="5px";
-    text.innerHTML  = "<p> Today you'll write a page where the user can type text into a box, and by clicking on UI controls, the user can decorate the text by giving it funny styling." +
+    text.innerHTML = "<p> Today you'll write a page where the user can type text into a box, and by clicking on UI controls, the user can decorate the text by giving it funny styling." +
         " </p> <p>The HTML page decoratemytext.html contains a basic HTML shell and page header. This skeleton already links to a CSS file decoratemytext.css that defines all the styles you need. You do not have to write any CSS code today.\n" +
         "\n</p> <p>You will write a JavaScript file decoratemytext.js that will manipulate the text.\n" +
         "\n</p><p> Download the HTML file below (right-click, Save Target As...) to get started:\n" +
-        "\n</p> <ul><li><a href='http://mumstudents.org/cs472/2016-05-BL/Labs/6/decoratemytext.html'> decoratemytext.html</a> </li></ul>"
+        "\n</p> <ul><li><a href='decoratemytext.html'> decoratemytext.html</a> </li></ul>"
 
 
 }
+
 function pageThree() {
 
     var text = document.getElementById("txt");
-    text.style.fontSize="1.5em";
-    text.style.textAlign="left";
-    text.innerHTML  = "<p> (See example screenshot on next slide.)</p> <p>The first task is to expand decoratemytext.html by adding UI controls. Add HTML code for the following:\n</p>" +
-        "<ul><li>A field for users to enter large (multi-line) amounts of text.\n</li><ul><li> It should be 4 rows by 30 columns in size.</li><li>Wrap it in a bordered field set box with the label \"Text\".\n</li></ul></li>"+
+    text.style.fontSize = "1.5em";
+    text.style.textAlign = "left";
+    text.innerHTML = "<p> (See example screenshot on next slide.)</p> <p>The first task is to expand decoratemytext.html by adding UI controls. Add HTML code for the following:\n</p>" +
+        "<ul><li>A field for users to enter large (multi-line) amounts of text.\n</li><ul><li> It should be 4 rows by 30 columns in size.</li><li>Wrap it in a bordered field set box with the label \"Text\".\n</li></ul></li>" +
         // "Add a second bordered field set box labeled \\\"Decoration\\\" that contains the following controls:\\n<li> A button labeled: Bigger Decorations!\\n</li><li> A checkbox labeled \\\"Bling\\\"\\n</li></ul><li> <strong>NOTE: Controls are sometimes used in forms; but you must not use the form tag on your page.</strong></li></ul>\"+"
-"<li>Add a second bordered field set box labeled \"Decoration\" that contains the following controls:\n</li><ul><li> A button labeled: Bigger Decorations!\n</li><li>A checkbox labeled \"Bling\"\n</li></ul></li>" +
+        "<li>Add a second bordered field set box labeled \"Decoration\" that contains the following controls:\n</li><ul><li> A button labeled: Bigger Decorations!\n</li><li>A checkbox labeled \"Bling\"\n</li></ul></li>" +
 
         "  <li><strong> NOTE: Controls are sometimes used in forms; but you must not use the form tag on your page. </strong></li>"
+}
+
+
+function pageFour() {
+
+    var text = document.getElementById("txt");
+    text.innerHTML = "<p> Your page should look like this: </p>" +
+        "<img src='output1-controls.png' alt='asdsa' />";
+    text.style.cssFloat = "left";
+}
+
+function pageFive() {
+
+    var text = document.getElementById("txt");
+    text.innerHTML = "<p> Now you'll write a bit of JavaScript testing code that pops up an alert box. This is just a test to make sure that your browser is running your JavaScript file, before we move on to tougher exercises.</p>" +
+        "<img src='output1-controls.png' alt='asdsa' />";
+    text.style.cssFloat = "left";
 }
 
 function change(x) {
@@ -55,10 +71,10 @@ function change(x) {
             pageThree();
             break;
         case 4:
-            pageZero();
+            pageFour();
             break;
         case 5:
-            pageOne();
+            pageFive();
             break;
         case 6:
             pageTwo();
