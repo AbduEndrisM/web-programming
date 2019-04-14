2ccc550101c61ee0635c0935885162fd67f8fae8
AbduEndrisM <beazsasm@gmail.com>
2019-04-14 18:46:36 -0500

    Initial commit

--- Homeworks/LessonFive/lab5.js
+++
@@ -1,6 +0,0 @@
-<!--Define a function max() that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in Javascript.-->
-
-
-function max() {
-    
-}
