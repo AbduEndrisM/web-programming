9994eaa727c603f4195d098bb2f08a57ac925e42
AbduEndrisM <beazsasm@gmail.com>
2019-04-14 17:08:34 -0500

    Lesson Five - Home work - JavaScript - basic to advanced and Unit tests

---
+++ Homeworks/LessonFive/lab5.js
@@ -0,0 +1,6 @@
+<!--Define a function max() that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in Javascript.-->
+
+
+function max() {
+    
+}
