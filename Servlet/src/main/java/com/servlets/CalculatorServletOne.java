c2450f73ecdcc905545fa4755433e763bc8285b4
AbduEndrisM <beazsasm@gmail.com>
2019-04-17 21:48:02 -0500

    Servlet - Lesson One - Simple Calculator using only Servlet

--- Servlet/src/main/java/com/servlets/CalculatorServletOne.java
+++ Servlet/src/main/java/com/servlets/CalculatorServletOne.java
@@ -15,6 +15,12 @@ public class CalculatorServletOne extends HttpServlet {
 
         PrintWriter out = res.getWriter();
 
+
+        out.println("<!DOCTYPE html\">\n" +
+                "<html> <head> <title> Lab One - Calculator </title> </head> \n" +
+                "<body> \n" +
+                "<font size=\"12px\" color=\"" + "blue" + "\">");
+
         try {
             res.setContentType("text/html");
             String num1 = req.getParameter("num1");
@@ -22,17 +28,22 @@ public class CalculatorServletOne extends HttpServlet {
             String num3 = req.getParameter("num3");
             String num4 = req.getParameter("num4");
 
-            if (!("".equals(num1)) && ("".equals(num2))) {
+            if (!("".equals(num1) && "".equals(num2))) {
 
-                out.println(num1 + " + " + num2 + " = " + Integer.parseInt(num1) + Integer.parseInt(num2));
+                out.println(num1 + " + " + num2 + " = " + (Integer.parseInt(num1) + Integer.parseInt(num2)) + "\n");
+                out.println("<br>");
             }
 
-            if (!("".equals(num3)) && ("".equals(num4))) {
-                out.println(num3 + " + " + num4 + " = " + Integer.parseInt(num3) + Integer.parseInt(num4));
+            if (!("".equals(num3) && "".equals(num4))) {
+
+                out.println(num3 + " * " + num4 + " = " + (Integer.parseInt(num3) * Integer.parseInt(num4)) + "\n");
             }
+
 
         } catch (Exception e) {
             out.println("Something is going wrong ...");
         }
+
+        out.println("</font>  </body> </html>" );
     }
 }
--- Servlet/src/main/java/com/servlets/CalculatorServletTwo.java
+++ Servlet/src/main/java/com/servlets/CalculatorServletTwo.java
@@ -9,33 +9,39 @@ import java.io.PrintWriter;
 //import javax.servlet.annotation.WebServlet;
 
 
-//@WebServlet("/calculatorTwo")
 public class CalculatorServletTwo extends HttpServlet {
     public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
 
         PrintWriter out = res.getWriter();
+
+
+        out.println("<!DOCTYPE html\">\n" +
+                "<html> <head> <title> Lab One - Calculator </title> </head> \n" +
+                "<body>");
+
         try {
             res.setContentType("text/html");
-
             String num1 = req.getParameter("num1");
             String num2 = req.getParameter("num2");
             String num3 = req.getParameter("num3");
             String num4 = req.getParameter("num4");
 
-
-            if (!("".equals(num1)) && ("".equals(num2))) {
+            if (!("".equals(num1) && "".equals(num2))) {
                 int sum = Integer.parseInt(num1) + Integer.parseInt(num2);
-//                out.println(num1 + " + " + num2 + " = " + Integer.parseInt(num1) + Integer.parseInt(num2));
-                out.println("  <input name=\"num1\" value=" + num1 + "> " + " <input name=\"num1\" value=" + num2 + "> +   <input type=\"text\" name=\"result1\" readonly value=" + sum + " >");
+                out.println("<input value="+num1+">" + " + " + "<input value="+num2+">" + " = " + "<input value="+sum+" readonly>" );
+                out.println("<br>");
             }
 
-            if (!("".equals(num3)) && ("".equals(num4))) {
-                out.println(num3 + " + " + num4 + " = " + Integer.parseInt(num3) + Integer.parseInt(num4));
+            if (!("".equals(num3) && "".equals(num4))) {
+                int prod = Integer.parseInt(num3) * Integer.parseInt(num4);
+                out.println("<input value="+num3+">" + " * " + "<input value="+num4+">" + " = " + "<input value="+prod+" readonly>" );
+                out.println("<br>");
             }
-
 
         } catch (Exception e) {
             out.println("Something is going wrong ...");
         }
+
+        out.println("</body> </html>" );
     }
 }
